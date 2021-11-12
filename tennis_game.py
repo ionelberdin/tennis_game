998331a617cec33dbf18408570b22384343c8ed1
Ion <ionelberdin@gmail.com>
2021-11-12 21:29:50 +0100

    Canvas is not part of ttk

--- tennis_game.py
+++ tennis_game.py
@@ -5,8 +5,8 @@ def tennis_game():
     root = Tk()
     content = ttk.Frame(root)
     frame = ttk.Frame(content, borderwidth=3)
-    canvas = ttk.Canvas(frame, width=400, height=800, background='grey')
-    tennis_court = ttk.Canvas(canvas, width=300, height=700, left=50, top=50, background='blue')
+    canvas = Canvas(frame, width=400, height=800, background='grey')
+    tennis_court = Canvas(canvas, width=300, height=700, left=50, top=50, background='blue')
     root.mainloop()
 
 if __name__ == '__main__':
