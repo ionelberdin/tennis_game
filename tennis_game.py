ecc2f98d4da60c4d0a4582a00e3bfdb34b29b358
Ion <ionelberdin@gmail.com>
2021-11-12 21:20:08 +0100

    first try

---
+++ tennis_game.py
@@ -0,0 +1,13 @@
+from tkinter import *
+from tkinter import ttk
+
+def tennis_game():
+    root = Tk()
+    content = ttk.Frame(root)
+    frame = ttk.Frame(content, borderwidth=3)
+    canvas = ttk.Canvas(frame, width=400, height=800, background='grey')
+    tennis_court = ttk.Canvas(canvas, width=300, height=700, left=50, top=50, background='blue')
+    root.mainloop()
+
+if __name__ == '__main__':
+    tennis_game()
