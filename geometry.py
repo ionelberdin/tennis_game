e668d5d7262e4c3a543b30f005e7b73dd511a951
Ion Elberdin Navarro <ionelberdin@gmail.com>
2021-11-16 20:57:53 +0100

    tennis court real dimentions

---
+++ geometry.py
@@ -0,0 +1,21 @@
+class Point:
+    def __init__(self, x, y=0, z=0):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    @property
+    def xy(self):
+        return (self.x, self.y)
+
+    @property
+    def xyz(self):
+        return (*self.xy, self.z)
+    
+    def __sum__(self, other):
+        return Point(*[i + j for i, j in zip(self.xyz, other.xyz)])
+
+
+class Line:
+    def __init__(self, points):
+        self.points = points
---
+++ tennis_court.py
@@ -0,0 +1,16 @@
+m_per_ft = 0.3048
+m_per_in = 0.0254
+
+# Tennis Court lengths in feet or inches, then converted to meters
+doubles_alley_width = 4.5 * m_per_ft
+court_width = 36 * m_per_ft
+court_length = 78 * m_per_ft
+service_box_length = 21 * m_per_ft
+center_mark_length = 4 * m_per_in  # 4 inches
+net_post_distance = 3 * m_per_ft  # distance to sideline
+net_post_height = 3.5 * m_per_ft
+net_central_height = 3 * m_per_ft
+net_central_strap_width = 2 * m_per_in  # 2 inches
+net_post_width = 6 * m_per_in  # 6 inches
+backstop_margin = 21 * m_per_ft
+lateral_margin = 12 * m_per_ft
